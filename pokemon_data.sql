644c459ce151eef2933a35b42c181b1d08935ecd
BooTeaa <112839822+Boo712@users.noreply.github.com>
2024-12-11 09:55:05 +0700

    Update pokemon_data.sql

--- pokemon_data.sql
+++ pokemon_data.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS pokemon;
 USE pokemon;
 
 CREATE TABLE pokemon (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     type1 VARCHAR(50),
     type2 VARCHAR(50),
@@ -15,9 +15,8 @@ CREATE TABLE pokemon (
     sp_def INT,
     speed INT,
     generation INT,
-    legendary TINYINT(1)
+    legendary BOOLEAN
 );
-
 
 LOAD DATA LOCAL INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\pokemon.csv' 
 INTO TABLE pokemon
@@ -26,7 +25,6 @@ OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (id, name, type1, type2, total, hp, attack, defense, sp_atk, sp_def, speed, generation, @legendary)
-SET legendary = IF(@legendary = 'TRUE', 1, IF(@legendary = 'FALSE', 0, NULL));
+SET legendary = IF(@legendary = 'TRUE', TRUE, IF(@legendary = 'FALSE', FALSE, NULL));
 
-SELECT * FROM pokemon
-
+SELECT * FROM pokemon;
